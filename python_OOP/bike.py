c86d60d9787d6152d1bb52fd8084e932d0e4dd8d
tumanjongbt <tumanjongbt@users.noreply.github.com>
2018-10-14 12:36:43 -0400

    Creating a class and making instances from it

---
+++ python_OOP/bike.py
@@ -0,0 +1,31 @@
+# define class Bike
+class Bike:
+    # this method to run every time a new Bike object is instantiated
+    def __init__(self, price, max_speed):
+	# instance attributes 
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = 0 # set initial miles to 0 whenever a new instance is created
+
+    # display price, maximum speed and total miles for a single instance
+    def displayInfo(self):
+        print(f"Purchase price is: {self.price}. Maximum speed is {self.max_speed}.  Total miles is {self.miles}.")
+        return self
+    # Riding method of the calling instance
+    def ride(self, miles):
+        print(f"Riding: ", miles, "miles")
+        return self
+
+    # Reverse method of the calling instance
+    def reverse(self, miles):
+        print(f"Reversing: ", miles, "miles")
+        return self
+        
+#create an instance of the Bike class
+bike1 = Bike(200,"25mph")
+bike2 = Bike(300,"30mph")
+bike3 = Bike(400,"40mph")
+bike1.ride(3).reverse(1).displayInfo()
+bike2.ride(2).reverse(2).displayInfo()
+bike3.reverse(3).displayInfo()
+
