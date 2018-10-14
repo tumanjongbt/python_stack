8aa6469086176fb64418d197153b17c2e127860f
tumanjongbt <tumanjongbt@users.noreply.github.com>
2018-10-14 01:16:58 -0400

    use of random module to generate random numbers

---
+++ python_fundamentals/functions_intermediate.py
@@ -0,0 +1,6 @@
+import random
+print(random.random()) # returns a random floating number between 0.000 to 1.000
+print(random.random()*50) # returns a float between 0.000 to 50.000
+print(random.random()*100) # returns a float between 0.000 to 100.000
+print(random.randrange(50, 100)) # returns an integer between 50 to 100
+print(random.randrange(50, 500)) # returns an integer between 50 to 500
